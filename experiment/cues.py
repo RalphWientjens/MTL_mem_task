4baab7a62aebe5ad1435372e1402e315f6342a1a
RalphWientjens <ralphw713@hotmail.com>
2026-05-29 11:15:12 +0200

    updated cues

--- experiment/cues.py
+++ experiment/cues.py
@@ -2,15 +2,15 @@ import pandas as pd
 import os
 
 # define cues
-cue1 = "School"         # far away
-cue2 = "Park"           # spatial
-cue3 = "Restaurant"     # emotional - negative
-cue4 = "Supermarket"    # emotional - positive
+cue1 = "Primary School" # far away - spatial
+cue2 = "Campus"         # Recent - spatial
+cue3 = "Balcony"        # far away - emotional 
+cue4 = "Azalea"         # Recent - positive
 
 # make dataframe with cues and memory type to save as tsv file
 cues_df = pd.DataFrame({
     "cue": [cue1, cue2, cue3, cue4],
-    "memory_type": ["far_away", "spatial", "emotional_negative", "emotional_positive"],
+    "memory_type": ["distant - spatial", "recent - spatial", "distant - emotional", "recent - emotional"],
     "trial_nr": [1, 2, 3, 4]
 })
 
